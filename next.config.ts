6b91a7fe51203e6ac564ecf46f7a5cce85e48e33
Anurag Maurya <justanuragmaurya@gmail.com>
2025-09-22 23:49:12 +0530

    added admin page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["v3.fal.media","v3b.fal.media"], // add more if needed
+  },
 };
 
 export default nextConfig;
